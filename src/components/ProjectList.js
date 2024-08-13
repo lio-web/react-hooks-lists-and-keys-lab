f46b302f0efd8ea86bc89d1cc65569a1273cb904
lionard <leonard.muhati@student.moringaschool.com>
2024-08-13 17:07:21 +0300

    compelete

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,23 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
-  return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+ 
+  const projectItems = projects.map((project) => (
+    <ProjectItem
+      key={project.name}
+      name={project.name}
+      about={project.about}
+      technologies={project.technologies}
+    />
+  ))
+    return (
+      <div id="projects">
+        <h2>My Projects</h2>
+        <div id="project-list">{projectItems}</div>
     </div>
   );
 }
+
+
 
 export default ProjectList;
